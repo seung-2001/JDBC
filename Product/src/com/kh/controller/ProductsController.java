b4d1656bd5771d434e9b5ec7cb06cca0e3e298fe
seung-2001 <ssj01022734558@gmail.com>
2025-09-01 21:21:33 +0900

    homework

--- Product/src/com/kh/controller/ProductsController.java
+++ Product/src/com/kh/controller/ProductsController.java
@@ -1,5 +1,25 @@
 package com.kh.controller;
 
+import java.util.List;
+
+import com.kh.model.dao.ProductsDao;
+import com.kh.model.vo.Products;
+
 public class ProductsController {
+
+	public int insertProducts(Products p) {
+		int result = new ProductsDao().insertProducts(p);
+        return result;
+	}
+	
+	public List<Products> printAll() {
+        List<Products> products = new ProductsDao().printAll();
+        return products;
+    }
+	
+	public List<Products> findBySku(String keyword) {
+        List<Products> products = new ProductsDao().findBySku(keyword);
+        return products;
+    }
 	
 }
--- Product/src/com/kh/view/ProductsView.java
+++ Product/src/com/kh/view/ProductsView.java
@@ -1,8 +1,13 @@
 package com.kh.view;
 
+import java.util.List;
 import java.util.Scanner;
 
+import com.kh.controller.ProductsController;
+import com.kh.model.vo.Products;
+
 public class ProductsView {
+	private ProductsController pc = new ProductsController();
 	private Scanner sc = new Scanner(System.in);
 
 	public void mainMenu() {
@@ -18,14 +23,16 @@ public class ProductsView {
 
 			switch (menuNo) {
 			case 1:
-				printAll();
+				insertProduct();
 				break;
 			case 2:
+				printAll();
 				break;
 			case 3:
+				findBySku();
 				break;
 			case 9:
-				break;
+				return;
 			default:
 				System.out.println("서비스를 종료합니다.");
 				return;
@@ -35,10 +42,82 @@ public class ProductsView {
 
 	}
 
+	private void insertProduct() {
+		System.out.println("\n--- 상품 추가 ---");
+
+		System.out.print("상품 번호 입력 > ");
+		int productNo = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("상품명 입력 > ");
+		String productName = sc.nextLine();
+
+		System.out.print("SKU 입력(UNIQUE) > ");
+		String sku = sc.nextLine();
+
+		System.out.print("카테고리 입력 > ");
+		String category = sc.nextLine();
+
+		System.out.print("가격 입력 > ");
+		int price = sc.nextInt();
+
+		System.out.print("수량 입력 > ");
+		int quantity = sc.nextInt();
+		sc.nextLine();
+
+		Products p = new Products(productNo, productName, sku, category, price, quantity);
+
+		int result = pc.insertProducts(p);
+
+		if (result > 0) {
+			System.out.println("상품 등록이 성공적으로 완료되었습니다.");
+		} else {
+			System.out.println("상품 등록에 실패하셨습니다.");
+		}
+	}
+
 	private void printAll() {
-		System.out.println("========================================");
-		
-		
+		System.out.println("\n--- 상품 전체 조회 ---");
+		List<Products> products = pc.printAll();
+		if (products.isEmpty()) {
+			System.out.println("조회 결과가 없습니다.");
+		} else {
+			for (Products p : products) {
+				System.out.println("===============================");
+				System.out.println(p.getProduct_No() + "번 상품 정보");
+				System.out.print("상품명 : " + p.getProduct_Name() + ", ");
+				System.out.print("SKU : " + p.getSku() + ", ");
+				System.out.print("카테고리 : " + p.getCategory() + ", ");
+				System.out.print("가격 : " + p.getPrice() + ", ");
+				System.out.print("수량 : " + p.getQuantity() + ", ");
+				System.out.print("등록일 : " + p.getProduct_Date());
+				System.out.println();
+			}
+		}
+
+	}
+
+	private void findBySku() {
+		System.out.println("\n--- 식별코드(SKU) 검색 ---");
+		System.out.print("검색하실 상품의 식별코드를 입력해주세요. > ");
+		String sku = sc.nextLine();
+
+		List<Products> products = pc.findBySku(sku);
+		if (products.isEmpty()) {
+			System.out.println("조회 결과가 없습니다.");
+		} else {
+			for (Products p : products) {
+				System.out.println("===============================");
+				System.out.println(p.getProduct_No() + "번 상품 정보");
+				System.out.print("상품명 : " + p.getProduct_Name() + ", ");
+				System.out.print("SKU : " + p.getSku() + ", ");
+				System.out.print("카테고리 : " + p.getCategory() + ", ");
+				System.out.print("가격 : " + p.getPrice() + ", ");
+				System.out.print("수량 : " + p.getQuantity() + ", ");
+				System.out.print("등록일 : " + p.getProduct_Date());
+				System.out.println();
+			}
+		}
 	}
 
 }
